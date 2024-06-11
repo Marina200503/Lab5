68f5dc063a2155b2917b967594da81496b77511d
Shiny <89213929454@mail.ru>
2024-06-11 09:21:01 +0300

    zczsc

--- src/main/java/utility/Executable.java
+++
@@ -1,7 +0,0 @@
-package utility;
-public interface Executable {
-    /**
-     * @param args - аргументы команды
-     */
-    boolean execute(String[] args);
-}
--- src/main/java/utility/Validatable.java
+++
@@ -1,7 +0,0 @@
-package utility;
-
-public interface Validatable {
-    //интерфейс классов для проверки валидности полей
-
-    boolean validate();
-}
